f573dd42219e3518ed907504271a4f1f0c26e851
Steve Krenzel <sgk284@gmail.com>
2018-02-23 13:35:52 -0800

    Hinting at array length.

--- src/uncrunch.js
+++ src/uncrunch.js
@@ -4,6 +4,7 @@ module.exports = function uncrunch(values) {
   const expanded = [];
   const lookup = (i => expanded[i]);
 
+  expanded.length = values.length; // Hint at array length for perf boost.
   for(let i = 0; i < values.length; i++) {
     const value = values[i];
     expanded[i] = isArray(value) ? value.map(lookup)
