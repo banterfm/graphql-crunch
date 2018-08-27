08a5b3d7173c75bd595892bf905c49e366d57cac
Steve Krenzel <sgk284@gmail.com>
2018-08-26 20:15:39 -0700

    Adding faster toKey method.

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
 const isArray = Array.isArray;
 
+function isString(x) {
+  return typeof(x) === 'string';
+}
+
 function isObject(value) {
   return (typeof value === 'object' && value !== null);
 }
@@ -17,8 +21,12 @@ function mapObject(object, fn) {
 }
 
 function toKey(value) {
-  return isObject(value) ? JSON.stringify(value, Object.keys(value).sort())
-                         : JSON.stringify(value);
+  // We know arrays will always only contain integers, so we can optimize
+  // their serialization.
+  return isArray(value) ? `[${value}]` :
+         isString(value) ? `"${value}"` :
+         isObject(value) ? JSON.stringify(value, Object.keys(value).sort()) :
+         value;
 }
 
 module.exports = {isArray, isObject, isUndefined, mapObject, toKey};
